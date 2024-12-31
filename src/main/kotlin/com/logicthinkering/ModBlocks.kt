0b5b20ec9a708c1a67533e13016124919d9c2a2b
Hunter104 <000plagueinc@gmail.com>
2024-12-31 09:35:49 -0300

    simplifies block registration

--- src/main/kotlin/com/logicthinkering/ModBlocks.kt
+++ src/main/kotlin/com/logicthinkering/ModBlocks.kt
@@ -2,9 +2,7 @@ package com.logicthinkering
 
 import com.logicthinkering.LogicThinkering.MOD_ID
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
-import net.minecraft.block.AbstractBlock
 import net.minecraft.block.AbstractBlock.Settings
-import net.minecraft.block.Block
 import net.minecraft.block.Blocks
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
@@ -13,19 +11,20 @@ import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 
-private val logicGate = AndGateBlock(Settings.copy(Blocks.REPEATER))
+private val logicSettings = Settings.copy(Blocks.REPEATER)
+private val blocksToRegister = listOf(
+    OrGate(logicSettings) to "or_gate_block",
+    AndGate(logicSettings) to "and_gate_block",
+)
 
 fun registerBlocks() {
-    registerBlock(logicGate, "logic_gate_block")
-    ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register { it.add(logicGate) }
-}
-
-private fun registerBlock(block: Block, name: String, shouldRegisterItem : Boolean = true): Block? {
-    val id = Identifier.of(MOD_ID, name)
-    if (shouldRegisterItem) {
-        val blockItem = BlockItem(block, Item.Settings())
-        Registry.register(Registries.ITEM, id, blockItem)
+    val registeredBlocks = blocksToRegister.map { (block, name) ->
+        val id = Identifier.of(MOD_ID, name)
+        Registry.register(Registries.BLOCK, id, block).also {
+            Registry.register(Registries.ITEM, id, BlockItem(it, Item.Settings()))
+        }
     }
-
-    return Registry.register(Registries.BLOCK, id, block)
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register {
+        registeredBlocks.forEach { block -> it.add(block) }
+    }
 }
