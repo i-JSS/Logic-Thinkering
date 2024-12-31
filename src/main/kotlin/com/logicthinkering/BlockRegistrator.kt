9058b647d8cbd57f4487a3309575d682bf14f433
Hunter104 <000plagueinc@gmail.com>
2024-12-31 20:38:30 -0300

    adds builder for blockregistration

---
+++ src/main/kotlin/com/logicthinkering/BlockRegistrator.kt
@@ -0,0 +1,61 @@
+package com.logicthinkering
+
+import com.logicthinkering.LogicThinkering.MOD_ID
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.minecraft.block.AbstractBlock.Settings
+import net.minecraft.block.Block
+import net.minecraft.block.Blocks
+import net.minecraft.item.BlockItem
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemGroups
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.util.Identifier
+
+fun registerBlocks(init: BlockRegistryBuilder.() -> Unit) {
+    val builder = BlockRegistryBuilder()
+    builder.init()
+    builder.register()
+}
+
+typealias BlockInit = (Settings) -> Block
+class BlockRegistryBuilder {
+    private val blocks = mutableListOf<Pair<BlockInit, String>>()
+    private var itemGroup : RegistryKey<ItemGroup> = ItemGroups.REDSTONE
+    private var settings : Settings? = null
+    private var registerItems : Boolean = true
+
+    fun settings(settings: Settings): BlockRegistryBuilder {
+        this.settings = settings
+        return this
+    }
+
+    fun group(group: RegistryKey<ItemGroup>): BlockRegistryBuilder {
+        itemGroup = group
+        return this
+    }
+
+    infix fun BlockInit.with(name: String) {
+        blocks += this to name
+    }
+
+    fun register() {
+        if (settings == null)
+            throw IllegalStateException("Settings must be set before registration")
+
+        val constructedBlocks = blocks.map { it.first(settings!!) to it.second}
+        val registeredBlocks = constructedBlocks.map { (block, name) ->
+            val id = Identifier.of(MOD_ID, name)
+            Registry.register(Registries.BLOCK, id, block).also {
+                if (registerItems) {
+                    Registry.register(Registries.ITEM, id, BlockItem(it, Item.Settings()))
+                }
+            }
+        }
+        ItemGroupEvents.modifyEntriesEvent(itemGroup).register {
+            registeredBlocks.forEach { block -> it.add(block) }
+        }
+    }
+}
--- src/main/kotlin/com/logicthinkering/ModBlocks.kt
+++ src/main/kotlin/com/logicthinkering/ModBlocks.kt
@@ -1,35 +1,20 @@
 package com.logicthinkering
 
-import com.logicthinkering.LogicThinkering.MOD_ID
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import com.logicthinkering.digitalcircuits.*
 import net.minecraft.block.AbstractBlock.Settings
 import net.minecraft.block.Blocks
-import net.minecraft.item.BlockItem
-import net.minecraft.item.Item
 import net.minecraft.item.ItemGroups
-import net.minecraft.registry.Registries
-import net.minecraft.registry.Registry
-import net.minecraft.util.Identifier
-
-private val logicSettings = Settings.copy(Blocks.REPEATER)
-private val blocksToRegister = listOf(
-    ORGate(logicSettings) to "or_gate_block",
-    ANDGate(logicSettings) to "and_gate_block",
-    XORGate(logicSettings) to "xor_gate_block",
-    NOTGate(logicSettings) to "not_gate_block",
-    NORGate(logicSettings) to "nor_gate_block",
-    NANDGate(logicSettings) to "nand_gate_block",
-    XNORGate(logicSettings) to "xnor_gate_block",
-)
 
 fun registerBlocks() {
-    val registeredBlocks = blocksToRegister.map { (block, name) ->
-        val id = Identifier.of(MOD_ID, name)
-        Registry.register(Registries.BLOCK, id, block).also {
-            Registry.register(Registries.ITEM, id, BlockItem(it, Item.Settings()))
-        }
-    }
-    ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register {
-        registeredBlocks.forEach { block -> it.add(block) }
+    registerBlocks {
+        group(ItemGroups.REDSTONE)
+        settings(Settings.copy(Blocks.REPEATER))
+        ::ORGate with "or_gate_block"
+        ::ANDGate with "and_gate_block"
+        ::XORGate with "xor_gate_block"
+        ::NOTGate with "not_gate_block"
+        ::NORGate with "nor_gate_block"
+        ::NANDGate with "nand_gate_block"
+        ::XNORGate with "xnor_gate_block"
     }
 }
