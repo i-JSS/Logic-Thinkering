7283ffa3fbdc0f048382376890944eac8385c9a8
Hunter104 <000plagueinc@gmail.com>
2024-12-31 21:20:48 -0300

    fix: fixes XNOR gate not having parenthesis

--- src/main/kotlin/com/logicthinkering/digitalcircuits/XNORGate.kt
+++ src/main/kotlin/com/logicthinkering/digitalcircuits/XNORGate.kt
@@ -5,6 +5,6 @@ import net.minecraft.block.AbstractBlock
 
 private val XNOR_CODEC = AbstractBlock.createCodec(::NANDGate)
 
-class XNORGate(settings: Settings) : AbstractLogicGate(settings, { !it.west xor it.east }) {
+class XNORGate(settings: Settings) : AbstractLogicGate(settings, { !(it.west xor it.east) }) {
     override fun getCodec(): MapCodec<NANDGate> = XNOR_CODEC
 }
